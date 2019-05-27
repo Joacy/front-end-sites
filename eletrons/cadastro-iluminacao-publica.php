06eadf52cb8839b1a76139b4d16a95ae732b1f51
JoacyMS <joacymsilva@gmail.com>
2019-05-27 19:02:01 -0300

    Escopo das páginas do site da Eletrons

---
+++ eletrons/cadastro-iluminacao-publica.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt_BR">
+
+<?php include('head.php'); ?>
+
+<body>
+    <div class="wrapper">
+
+        <?php include('header.php'); ?>
+
+        <!-- Conteúdo -->
+        
+        <!-- Fim do Conteúdo -->
+        
+        <?php include('footer.php'); ?>
+
+    </div>
+    <?php include('script.php'); ?>
+</body>
+</html>
