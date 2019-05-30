d9f0e12c2c1c091ff0ce60912c38d3efccdcd250
JoacyMS <joacymsilva@gmail.com>
2019-05-30 15:37:51 -0300

    Página de Sobre Nós do site da Eletrons

--- eletrons/sobre-nos.php
+++ eletrons/sobre-nos.php
@@ -4,17 +4,343 @@
 <?php include('head.php'); ?>
 
 <body>
-    <div class="wrapper">
+	<div class="wrapper">
 
-        <?php include('header.php'); ?>
+		<?php include('header.php'); ?>
 
-        <!-- Conteúdo -->
-        
-        <!-- Fim do Conteúdo -->
-        
-        <?php include('footer1.php'); ?>
+		<!-- Conteúdo -->
+		<section class="principal-sobre coluna-flex centralizado">
+			<div class="container">
+				<div class="row linha-principal">
+					<div class="col-12 col-md-6">
+						<div class="row linha-flex esquerda">
+							<h1 class="titulo-principal branco">
+								Especialistas em redução de perdas no setor de energia!
+							</h1>
+							<h2 class="texto-principal branco">
+								O segredo de sermos pioneiros nesse serviço é devido a nossa equipe altamente treinada e especializada para os diversos tipos de situação, e com uma tecnologia que desenvolvemos para atualizar o seu banco de dados automaticamente.
+							</h2>
+						</div>
+						<div class="row linha-flex esquerda">
+							<a class="btn botao-principal" href="">Quero reduzir as perdas</a>
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
 
-    </div>
-    <?php include('script.php'); ?>
-</body>
-</html>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="segunda-sobre">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<h2 class="titulo preto2">
+						Destaques
+					</h2>
+				</div>
+				<div class="linha-flex centralizado row">
+					<div class="col-9 col-md-4">
+						<div class="coluna-flex centralizado quadro-sobre">
+							<div class="icone">
+								<img class="icone-pequeno" src="svg/opinion.svg">
+							</div>
+							<div class="icone">	
+								<img src="svg/bitcoin.svg">
+							</div>
+							<div class="corpo-texto">
+								<h3 class="subtitulo preto2 negrito">
+									Experiência
+								</h3>
+
+								<p class="texto cinza">
+									Estamos no mercado a mais de nove anos, e com essa expertise nos tornamos especialistas em redução de custos; inovamos a cada dia para continuar sendo a melhor empresa do Brasil.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-9 col-md-4">
+						<div class="coluna-flex centralizado quadro-sobre">
+							<div class="icone">
+								<img src="svg/star.svg">
+							</div>
+							<div class="corpo-texto">
+								<h3 class="subtitulo preto2 negrito">
+									Recorde em Cadastramento Público
+								</h3>
+
+								<p class="texto cinza">
+									Somos a melhor solução em cadastramento público do Brasil, com um numero registrado de mais de 18 milhões de cadastros realizados.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-9 col-md-4">
+						<div class="coluna-flex centralizado quadro-sobre">
+							<div class="icone">
+								<img src="svg/dollar-symbol.svg">
+							</div>
+							<div class="corpo-texto">
+								<h3 class="subtitulo preto2 negrito">
+									Redução de Perdas
+								</h3>
+
+								<p class="texto cinza">
+									Sabemos como o número de perdas no setor elétrico é alto e por esse motivo estamos aqui com a melhor solução na redução desse problema.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="terceira-sobre">
+			<div class="container">
+
+				<div class="linha-flex centralizado">
+					<h2 class="titulo preto2">
+						Como Surgiu a Eletrons
+					</h2>
+				</div>
+
+				<div class="linha-flex borda-sobre">
+					<div class="retangulo">
+
+					</div>
+					<div>
+						<h3 class="subtitulo esquerda preto2 mb-15">
+							A Eletrons é uma empresa inovadora, parceira de concessionárias de energia elétrica e companhias elétricas, que presta serviços voltados para o ramo de serviços de Cadastro de Iluminação Pública, com levantamento de carga e traçado de perfil da rede secundária, com objetivo de medir o faturamento da Rede de Iluminação Pública - RIP. Focada na eficientização dos processos, aqui você encontra soluções completas com uma equipe altamente capacitada para o cadastro de sistemas de iluminação pública, a fim de possibilitar a recuperação de perdas na iluminação pública. 
+						</h3>
+						<h3 class="subtitulo esquerda preto2">
+							É unânime entre as distribuidoras de energia, que o cadastro de iluminação deve ser atualizado com intervalo de 2 anos, pois geralmente irregularidades acumuladas na iluminação pública geram um aumento superior da carga em 30%. É com essa visão que, diante deste potencial de prejuízos possíveis, fica evidente a importância de uma análise e diagnóstico da qualidade da energia elétrica.
+						</h3>
+					</div>
+				</div>
+
+				<div class="linha-flex centralizado">
+					<h2 class="titulo mt-15 preto2">
+						Nossa história
+					</h2>
+				</div>
+
+				<div class="linha-flex borda-sobre">
+					<h3 class="subtitulo esquerda preto2">
+						A paixão pela eletricidade levou a Eletrons a se tornar líder no mercado nacional. A marca iniciou sua história de sucesso há 10 anos, com sede em Feira de Santana atuando no mercado de Eletricidade e Cadastro de Iluminação Pública, sempre expandindo o seu mercado na Bahia e atuando também em outros Estados.
+					</h3>
+					<div class="retangulo1">
+
+					</div>
+				</div>
+
+				<div class="linha-flex centralizado">
+					<h2 class="titulo mt-15 preto2">
+						Nossa Missão
+					</h2>
+				</div>
+
+				<div class="linha-flex borda-sobre">
+					<div class="retangulo">
+
+					</div>
+					<h3 class="subtitulo esquerda preto2">
+						Ser a melhor e mais rentável empresa de Cadastro de Iluminação Pública do Brasil. Levando segurança e melhoria da qualidade dos sistemas de gestão na iluminação pública. Um dos principais objetivos da empresa é levar qualidade e satisfação aos clientes. Realizando o cadastramento e auditoria a fim de planejar, controlar e disciplinar todas as atividades relacionadas aos serviços de iluminação pública desenvolvendo a economia e o uso racional da energia elétrica nas vias públicas dos municípios, por meio da eficiência e controle referente ao consumo de energia adequado.
+					</h3>
+				</div>
+
+				<div class="linha-flex centralizado">
+					<h2 class="titulo mt-15 preto2">
+						Responsabilidade Ambiental
+					</h2>
+				</div>
+
+				<div class="linha-flex borda-sobre">
+					<h3 class="subtitulo esquerda preto2">
+						Numa iniciativa promovida pela equipe administrativa da Eletrons, está sendo realizada a primeira etapa migração para utilização de papéis reciclados com o intuito de ser mais uma empresa na contribuição da redução do desmatamento.
+					</h3>
+					<div class="retangulo1">
+
+					</div>
+				</div>
+
+				<div class="linha-flex centralizado">
+					<h2 class="titulo mt-15 preto2">
+						Responsabilidade Social
+					</h2>
+				</div>
+
+				<div class="linha-flex borda-sobre">
+					<div class="retangulo">
+
+					</div>
+					<h3 class="subtitulo esquerda preto2">
+						Um dos principais objetivos da empresa é melhorar a qualidade de vida nos municípios, realizando um trabalho eficiente e é graças a esse projeto inovador, que os benefícios para a população se traduzem na superior qualidade de vida e o Poder Público lucra financeiramente com a exploração adequada de seus recursos naturais.
+					</h3>
+				</div>
+
+
+				<div class="linha-flex centralizado">
+					<h2 class="titulo mt-15 preto2">
+						Por que nos contratar?
+					</h2>
+				</div>
+
+				<div class="linha-flex borda-sobre">
+					<ul>
+						<li>
+							<h3 class="subtitulo esquerda preto2">
+								Tradição no cadastro de IP e consumidores a mais de 12 anos;
+							</h3>
+						</li>
+						<li>
+							<h3 class="subtitulo esquerda preto2">
+								Mão de Obra qualificada e certificada;
+							</h3>
+						</li>
+						<li>
+							<h3 class="subtitulo esquerda preto2">
+								Segurança e qualidade na informação;
+							</h3>
+						</li>
+						<li>
+							<h3 class="subtitulo esquerda preto2">
+								Consultoria presencial e on-line;
+							</h3>
+						</li>
+						<li>
+							<h3 class="subtitulo esquerda preto2">
+								Sistema móbile de coleta em campo com coordenadas geográficas;
+							</h3>
+						</li>
+						<li>
+							<h3 class="subtitulo esquerda preto2">
+								Banco de dados on-line para consulta e geração de relatórios;
+							</h3>
+						</li>
+						<li>
+							<h3 class="subtitulo esquerda preto2">
+								Valor do serviço justo, com um ótimo custo benefício.
+							</h3>
+						</li>
+					</ul>
+					<div class="retangulo1">
+
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+		<section class="quarta-sobre">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<h2 class="titulo preto2">
+						Lorem ipsum
+					</h2>
+				</div>
+
+				<div class="row linha-flex centralizado">
+					<div class="coluna-flex centralizado col-12 col-md-6">
+						<div class="quadro-servicos-sobre row">
+							<div class="col-3 coluna-flex centralizado icone">
+								<img src="svg/thumbs-up.svg">
+							</div>
+							<div class="col-9 coluna-flex centralizado">
+								<div class="linha-flex esquerda">
+									<h3 class="subtitulo preto2 negrito">
+										Cadastro de Iluminação Pública
+									</h3>
+								</div>
+								<div class="linha-flex esquerda">
+									<p class="texto cinza">
+										A cada R$ 1,00 investido você recupera R$ 5,00, isso acontece através de uma atualização completa do seu banco de dados.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-md-6">
+						<div class="quadro-servicos-sobre row">
+							<div class="col-3 coluna-flex centralizado icone">
+								<img src="svg/dollar-symbol.svg">
+							</div>
+							<div class="col-9 coluna-flex centralizado">
+								<div class="linha-flex esquerda">
+									<h3 class="subtitulo preto2 negrito">
+										Uso Mútuo
+									</h3>
+								</div>
+								<div class="linha-flex esquerda">
+									<p class="texto cinza">
+										A cada R$ 1,00 investido você recupera R$ 6,00, isso acontece através de um mapeamento e atualização do seu banco de dados com a identificação das empresas que estão utilizando seus postes e não estão pagando por isso.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-md-6">
+						<div class="quadro-servicos-sobre row">
+							<div class="col-3 coluna-flex centralizado icone">
+								<img src="svg/rising.svg">
+							</div>
+							<div class="col-9 coluna-flex centralizado">
+								<div class="linha-flex esquerda">
+									<h3 class="subtitulo preto2 negrito">
+										Ligação Clandestina
+									</h3>
+								</div>
+								<div class="linha-flex esquerda">
+									<p class="texto cinza">
+										Em média o mapeamento georeferenciado das ligações clandestinas apresentam um retorno de 90% no desligamento do fornecimento sem medição e 75% nas regularizações dos consumidores que foram desligados.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-md-6">
+						<div class="quadro-servicos-sobre row">
+							<div class="col-3 coluna-flex centralizado icone">
+								<img src="svg/around-the-world2.svg">
+							</div>
+							<div class="col-9 coluna-flex centralizado">
+								<div class="linha-flex esquerda">
+									<h3 class="subtitulo preto2 negrito">
+										Cadastro de Ativo (Rede de Distribuição)
+									</h3>
+								</div>
+								<div class="linha-flex esquerda">
+									<p class="texto cinza">
+										O levantamento e mapeamento cadastral de toda rede de distribuição irá lhe permitir ter uma base consistente do seu ativo em campo, e assim possibilitar a atualização de outras bases, como clientes, iluminação pública, uso mútuo.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="quinta-sobre">
+				<div class="container">
+					<div class="linha-flex centralizado">
+						<h2 class="titulo preto2">
+							Saiba mais sobre nós
+						</h2>
+					</div>
+					<div class="linha-flex centralizado">
+						<p class="texto cinza">
+							Entre em contato!
+						</p>
+					</div>
+					<div class="linha-flex centralizado">
+						<a class="btn botao-contato" href="contato.php">Contato</a>
+					</div>
+				</div>
+			</section>
+			<!-- Fim do Conteúdo -->
+
+			<?php include('footer1.php'); ?>
+
+		</div>
+		<?php include('script.php'); ?>
+	</body>
+	</html>
