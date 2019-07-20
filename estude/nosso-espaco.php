d3caaa7bc3d8d2831621eb8bbde3008e23e200b4
JoacyMS <joacymsilva@gmail.com>
2019-07-20 14:24:57 -0300

    Página de 'Nosso Espaço' do Site da Estude

--- estude/nosso-espaco.php
+++ estude/nosso-espaco.php
@@ -9,7 +9,443 @@
 		<?php include('header.php'); ?>
 
 		<!-- Conteúdo -->
-		
+		<section class="coluna-flex centralizado principal" data-page="nosso-espaco">
+			<div class="d-none d-lg-block container">
+				<div class="row">
+					<div class="coluna-flex justify-content-center col-12 col-lg-6">
+						<div>
+							<p class="linha-flex start texto-esquerda titulo-principal branco black">
+								Chega de<br>distrações!
+							</p>
+							<a class="btn botao-principal" href="contato.php">Quero reservar!</a>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-lg-6">
+						<!-- <div class="linha-flex centralizado">
+							<img src="imagens/1.png">
+						</div> -->
+					</div>
+				</div>
+			</div>
+			<div class="d-none d-lg-block linha-flex centralizado foto">
+				<img class="oculos-fone" src="imagens/1.png">
+			</div>
+
+			<div class="d-lg-none container">
+				<div class="row">
+					<div class="coluna-flex align-items-center justify-content-start col-12 col-lg-6">
+						<div>
+							<p class="linha-flex start texto-esquerda titulo-principal branco black">
+								Chega de<br>distrações!
+							</p>
+							<a class="btn botao-principal" href="contato.php">Quero reservar!</a>
+						</div>
+					</div>
+					<!-- <div class="coluna-flex centralizado col-12 col-lg-6 ultima">
+						<div class="linha-flex centralizado">
+							<img src="imagens/1.png">
+						</div>
+					</div> -->
+				</div>
+			</div>
+			<div class="d-lg-none linha-flex centralizado foto1">
+				<img class="oculos-fone" src="imagens/1.png">
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado sec-nosso-espaco">
+			<img class="bg1" src="imagens/ascasdsdas.png">
+			<img class="bg2" src="imagens/ascasdsdas.png">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<p class="titulo preto black">
+						Nosso espaço
+					</p>
+				</div>
+				<div class="linha-flex centralizado">
+					<img src="imagens/Rectangle 58.png">
+				</div>
+				<div class="linha-flex align-items-center texto-esquerda">
+					<p class="texto preto regular">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet sem sodales arcu iaculis tincidunt a sit amet est. Nam in libero eget lacus fringilla mollis a et augue. Curabitur erat libero, vestibulum sit amet auctor non, viverra vitae tellus. Aenean molestie tellus sit amet tincidunt mattis. Nam in libero eget lacus fringilla mollis a et augue. Curabitur erat libero, vestibulum sit amet auctor non, viverra vitae tellus. Aenean molestie tellus sit amet tincidunt mattis.
+					</p>
+				</div>
+			</div>
+		</section>
+
+
+		<section class="coluna-flex centralizado observacoes">
+			<div class="container">
+				<div class="linha-flex start">
+					<p class="linha-flex texto-esquerda titulo preto black">
+						Destaques da Estude
+					</p>
+					<svg class="d-none d-md-block">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+					<svg class="d-md-none">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+				</div>
+			</div>
+		</section>
+
+		<section class="sec-caixas">
+			<div class="container">
+				<div class="row">
+					<div class="coluna-flex centralizado col-12 col-lg-4">
+						<div class="coluna-flex centralizado caixa">
+							<div class="row">
+								<div class="coluna-flex centralizado col-3 col-lg-4">
+									<div class="linha-flex centralizado">
+										<img src="imagens/silent.svg">
+									</div>
+								</div>
+								<div class="coluna-flex centralizado col-9 col-lg-8">
+									<div>
+										<div class="linha-flex">
+											<p class="texto preto black">
+												Silêncio absoluto
+											</p>
+										</div>
+										<div class="linha-flex">
+											<p class="texto preto regular">
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-lg-4">
+						<div class="coluna-flex centralizado caixa">
+							<div class="row">
+								<div class="coluna-flex centralizado col-3 col-lg-4">
+									<div class="linha-flex centralizado">
+										<img src="imagens/star.svg">
+									</div>
+								</div>
+								<div class="coluna-flex centralizado col-9 col-lg-8">
+									<div>
+										<div class="linha-flex">
+											<p class="texto preto black">
+												O seu queridinho
+											</p>
+										</div>
+										<div class="linha-flex">
+											<p class="texto preto regular">
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-lg-4">
+						<div class="coluna-flex centralizado caixa">
+							<div class="row">
+								<div class="coluna-flex centralizado col-3 col-lg-4">
+									<div class="linha-flex centralizado">
+										<img src="imagens/open-book (1).svg">
+									</div>
+								</div>
+								<div class="coluna-flex centralizado col-9 col-lg-8">
+									<div>
+										<div class="linha-flex">
+											<p class="texto preto black">
+												Espaço preparado
+											</p>
+										</div>
+										<div class="linha-flex">
+											<p class="texto preto regular">
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="contratar">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<a class="btn botao-contratar" href="nossos-planos.php">Quero reservar!</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado sec-galeria">
+			<div class="coluna-flex centralizado container">
+				<img class="bg1" src="imagens/ascasdsdas.png">
+				<img class="bg2" src="imagens/ascasdsdas.png">
+				<div class="linha-flex centralizado">
+					<p class="titulo preto black">
+						Galeria
+					</p>
+				</div>
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-4">
+						<div class="linha-flex centralizado quadro-foto">
+							<div class="linha-flex centralizado">
+								<i class="btn fas fa-search-plus" data-toggle="modal" data-target="#foto1"></i>
+							</div>
+						</div>
+						<div class="linha-flex legenda-foto">
+							<p class="texto preto regular">
+								Quem somos
+							</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-4">
+						<div class="linha-flex centralizado quadro-foto">
+							<div class="linha-flex centralizado">
+								<i class="btn fas fa-search-plus" data-toggle="modal" data-target="#foto2"></i>
+							</div>
+						</div>
+						<div class="linha-flex legenda-foto">
+							<p class="texto preto regular">
+								Quem somos
+							</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-4">
+						<div class="linha-flex centralizado quadro-foto">
+							<div class="linha-flex centralizado">
+								<i class="btn fas fa-search-plus" data-toggle="modal" data-target="#foto3"></i>
+							</div>
+						</div>
+						<div class="linha-flex legenda-foto">
+							<p class="texto preto regular">
+								Quem somos
+							</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-4">
+						<div class="linha-flex centralizado quadro-foto">
+							<div class="linha-flex centralizado">
+								<i class="btn fas fa-search-plus" data-toggle="modal" data-target="#foto4"></i>
+							</div>
+						</div>
+						<div class="linha-flex legenda-foto">
+							<p class="texto preto regular">
+								Quem somos
+							</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-4">
+						<div class="linha-flex centralizado quadro-foto">
+							<div class="linha-flex centralizado">
+								<i class="btn fas fa-search-plus" data-toggle="modal" data-target="#foto5"></i>
+							</div>
+						</div>
+						<div class="linha-flex legenda-foto">
+							<p class="texto preto regular">
+								Quem somos
+							</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-4">
+						<div class="linha-flex centralizado quadro-foto">
+							<div class="linha-flex centralizado">
+								<i class="btn fas fa-search-plus" data-toggle="modal" data-target="#foto6"></i>
+							</div>
+						</div>
+						<div class="linha-flex legenda-foto">
+							<p class="texto preto regular">
+								Quem somos
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<img class="d-none d-xl-block semiCirculo" src="imagens/Group 23.png">
+		</section>
+
+		<div class="modal fade" id="foto1" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title texto preto bold" id="TituloModalCentralizado">Foto 1</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="linha-flex centralizado">
+							<img src="imagens/escudo.png">
+						</div>
+					</div>
+					<div class="coluna-flex centralizado modal-footer">
+						<div class="linha-flex centralizado">
+							<p class="texto preto regular">
+								Legenda da Imagem
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="foto2" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title texto preto bold" id="TituloModalCentralizado">Foto 2</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="linha-flex centralizado">
+							<img src="imagens/escudo.png">
+						</div>
+					</div>
+					<div class="coluna-flex centralizado modal-footer">
+						<div class="linha-flex centralizado">
+							<p class="texto preto regular">
+								Legenda da Imagem
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="foto3" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title texto preto bold" id="TituloModalCentralizado">Foto 3</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="linha-flex centralizado">
+							<img src="imagens/escudo.png">
+						</div>
+					</div>
+					<div class="coluna-flex centralizado modal-footer">
+						<div class="linha-flex centralizado">
+							<p class="texto preto regular">
+								Legenda da Imagem
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="foto4" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title texto preto bold" id="TituloModalCentralizado">Foto 4</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="linha-flex centralizado">
+							<img src="imagens/escudo.png">
+						</div>
+					</div>
+					<div class="coluna-flex centralizado modal-footer">
+						<div class="linha-flex centralizado">
+							<p class="texto preto regular">
+								Legenda da Imagem
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="foto5" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title texto preto bold" id="TituloModalCentralizado">Foto 5</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="linha-flex centralizado">
+							<img src="imagens/escudo.png">
+						</div>
+					</div>
+					<div class="coluna-flex centralizado modal-footer">
+						<div class="linha-flex centralizado">
+							<p class="texto preto regular">
+								Legenda da Imagem
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="foto6" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title texto preto bold" id="TituloModalCentralizado">Foto 6</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="linha-flex centralizado">
+							<img src="imagens/escudo.png">
+						</div>
+					</div>
+					<div class="coluna-flex centralizado modal-footer">
+						<div class="linha-flex centralizado">
+							<p class="texto preto regular">
+								Legenda da Imagem
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<section class="contratar">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<a class="btn botao-contratar" href="nossos-planos.php">Veja mais</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado contato-laranja">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<div>
+						<div class="linha-flex start">
+							<img class="superior" src="imagens/Path 13.svg">
+						</div>
+						<p class="linha-flex centralizado texto-centro titulo preto black">
+							Lorem ipsum dolor sit amet
+						</p>
+						<div class="linha-flex end">
+							<img class="inferior" src="imagens/Path 14.svg">
+						</div>
+					</div>
+				</div>
+
+				<div class="linha-flex centralizado">
+					<a class="btn botao-contratar" href="contato.php">CONTATO</a>
+				</div>
+			</div>
+		</section>
 		<!-- Fim do Conteúdo -->
 		
 		<?php include('footer.php'); ?>
