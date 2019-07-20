732511e2087a72d7910cddfc01a0b3079c45759a
JoacyMS <joacymsilva@gmail.com>
2019-07-20 14:21:37 -0300

    Página de 'Quem Somos' do Site da Estude

--- estude/quem-somos.php
+++ estude/quem-somos.php
@@ -9,9 +9,339 @@
 		<?php include('header.php'); ?>
 
 		<!-- Conteúdo -->
-		
+		<section class="coluna-flex centralizado principal" data-page="quem-somos">
+			<div class="d-none d-lg-block container">
+				<div class="row">
+					<div class="coluna-flex centralizado col-12 col-lg-6">
+
+					</div>
+					<div class="coluna-flex justify-content-center col-12 col-lg-6">
+						<div>
+							<p class="linha-flex start texto-esquerda titulo-principal branco black">
+								Afinal,<br>quem é Estude?
+							</p>
+							<div class="linha-flex end">
+								<a class="btn botao-principal end" href="contato.php">Agora sim!</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="d-none d-lg-block foto">
+				<img class="nerdao" src="imagens/Agrupar 1 (2).png">
+			</div>
+
+			<div class="d-lg-none container">
+				<div class="row">
+					<div class="coluna-flex centralizado col-12 col-lg-6">
+						<div>
+							<p class="linha-flex start texto-esquerda titulo-principal branco black">
+								Afinal,<br>quem é Estude?
+							</p>
+							<div class="linha-flex end">
+								<a class="btn botao-principal end" href="contato.php">Agora sim!</a>
+							</div>
+						</div>
+					</div>
+					<!-- <div class="coluna-flex centralizado col-12 col-lg-6 ultima">
+						
+					</div> -->
+				</div>
+			</div>
+			<div class="d-lg-none linha-flex centralizado foto1">
+				<img class="nerdao" src="imagens/Agrupar 1 (2).png">
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado quem-e-estude">
+			<div class="container">
+				<div class="linha-flex centralizado row">
+					<div class="col-12 col-lg-6">
+						<div class="linha-flex centralizado">
+							<div id="escudo1" class="d-none d-lg-block escudo wow slideInLeft" data-wow-duration="1s" data-wow-offset="300">
+								<img class="fundo wow rotateIn" data-wow-offset="300" src="imagens/escudo.png">
+							</div>
+							<div id="escudo1" class="d-lg-none escudo">
+								<img class="fundo" src="imagens/escudo.png">
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-lg-6">
+						<div class="linha-flex start">
+							<img class="superior" src="imagens/Path 13.svg">
+						</div>					
+						<p class="titulo preto black">
+							Afinal, quem é Estude?
+						</p>
+						<div class="linha-flex end">
+							<img class="inferior" src="imagens/Path 14.svg">
+						</div>
+					</div>
+				</div>
+				<div class="linha-flex">
+					<p class="texto preto regular">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet sem sodales arcu iaculis tincidunt a sit amet est. Nam in libero eget lacus fringilla mollis a et augue. Curabitur erat libero, vestibulum sit amet auctor non, viverra vitae tellus. Aenean molestie tellus sit amet tincidunt mattis. Nam in libero eget lacus fringilla mollis a et augue. Curabitur erat libero, vestibulum sit amet auctor non, viverra vitae tellus. Aenean molestie tellus sit amet tincidunt mattis.
+					</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado observacoes">
+			<div class="container">
+				<div class="linha-flex start">
+					<p class="linha-flex texto-esquerda titulo preto black">
+						lorem ipsum dolor sit
+					</p>
+					<svg class="d-none d-md-block">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+					<svg class="d-md-none">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+				</div>
+			</div>
+		</section>
+
+		<section class="livros">
+			<div class="container">
+				<div class="row linha-livros">
+					<div class="coluna-flex centralizado col-12 col-md-4">
+						<div class="row">
+							<div class="col-3 col-md-4 col-xl-3">
+								<div class="linha-flex centralizado">
+									<img src="imagens/open-book.svg">
+								</div>
+							</div>
+							<div class="col-9 col-md-8 col-xl-9">
+								<div class="linha-flex">
+									<p class="texto preto black">
+										lorem ipsum dolor sit
+									</p>
+								</div>
+								<div class="linha-flex">
+									<p class="texto cinza regular">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-md-4">
+						<div class="row">
+							<div class="col-3 col-md-4 col-xl-3">
+								<div class="linha-flex centralizado">
+									<img src="imagens/open-book.svg">
+								</div>
+							</div>
+							<div class="col-9 col-md-8 col-xl-9">
+								<div class="linha-flex">
+									<p class="texto preto black">
+										lorem ipsum dolor sit
+									</p>
+								</div>
+								<div class="linha-flex">
+									<p class="texto cinza regular">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="coluna-flex centralizado col-12 col-md-4">
+						<div class="row">
+							<div class="col-3 col-md-4 col-xl-3">
+								<div class="linha-flex centralizado">
+									<img src="imagens/open-book.svg">
+								</div>
+							</div>
+							<div class="col-9 col-md-8 col-xl-9">
+								<div class="linha-flex">
+									<p class="texto preto black">
+										lorem ipsum dolor sit
+									</p>
+								</div>
+								<div class="linha-flex">
+									<p class="texto cinza regular">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="contratar">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<a class="btn botao-contratar" href="nosso-espaco.php">Conheça o espaço!</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado observacoes">
+			<div class="container">
+				<div class="linha-flex start">
+					<p class="linha-flex texto-esquerda titulo preto black">
+						lorem ipsum dolor sit
+					</p>
+					<svg class="d-none d-md-block">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+					<svg class="d-md-none">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+				</div>
+			</div>
+		</section>
+
+		<section class="coluna-flex justify-content-center secao-fotos">
+			<div class="container">
+				<div class="linha-flex row">
+					<div class="col-12 col-md-4">
+						<div class="linha-flex centralizado">
+							<img src="imagens/fundo-branco.jpg">
+						</div>
+					</div>
+					<div class="col-12 col-md-4">
+						<div class="linha-flex centralizado">
+							<img src="imagens/fundo-branco.jpg">
+						</div>		
+					</div>
+					<div class="col-12 col-md-4">
+						<div class="coluna-flex">
+							<div class="linha-flex centralizado">
+								<img src="imagens/fundo-branco.jpg">
+							</div>
+						</div>
+						<div class="coluna-flex ultima">
+							<div class="linha-flex centralizado">
+								<img src="imagens/fundo-branco.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="contratar">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<a class="btn botao-contratar" href="nosso-espaco.php">Veja mais</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado observacoes">
+			<div class="container">
+				<div class="linha-flex start">
+					<p class="linha-flex texto-esquerda titulo preto black">
+						lorem ipsum dolor sit
+					</p>
+					<svg class="d-none d-md-block">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+					<svg class="d-md-none">
+						<line x1="0" y1="50%" x2="100%" y2="50%"/>
+					</svg>
+				</div>
+			</div>
+		</section>
+
+		<section class="quadro-comentario-quem-somos">
+			<div class="container">
+				<div class="row">
+					<div class="coluna-flex centralizado col-12 col-lg-6">
+						<div class="quadro">
+							<div class="linha-flex">
+								<div class="coluna-flex centralizado imagem">
+									<img src="imagens/Ellipse 10.svg">
+								</div>
+								<div class="coluna-flex start texto-esquerda info">
+									<p class="nome preto black">
+										lorem ipsum dolor sit
+									</p>
+									<p class="cargo cinza regular">
+										Lorem ipsum dolor sit
+									</p>
+								</div>
+							</div>
+							<div class="linha-flex start">
+								<p class="legenda cinza regular">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								</p>
+							</div>
+						</div>				
+					</div>
+
+					<div class="coluna-flex centralizado col-12 col-lg-6">
+						<div class="quadro">
+							<div class="linha-flex">
+								<div class="coluna-flex centralizado imagem">
+									<img src="imagens/Ellipse 10.svg">
+								</div>
+								<div class="coluna-flex start texto-esquerda info">
+									<p class="nome preto black">
+										lorem ipsum dolor sit
+									</p>
+									<p class="cargo cinza regular">
+										Lorem ipsum dolor sit
+									</p>
+								</div>
+							</div>
+							<div class="linha-flex start">
+								<p class="legenda cinza regular">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="coluna-flex centralizado contato-laranja">
+			<div class="container">
+				<div class="linha-flex centralizado">
+					<div>
+						<div class="linha-flex start">
+							<img class="superior" src="imagens/Path 13.svg">
+						</div>
+						<p class="linha-flex centralizado texto-centro titulo preto black">
+							Lorem ipsum dolor sit amet
+						</p>
+						<div class="linha-flex end">
+							<img class="inferior" src="imagens/Path 14.svg">
+						</div>
+					</div>
+				</div>
+
+				<div class="linha-flex centralizado">
+					<a class="btn botao-contratar" href="contato.php">CONTATO</a>
+				</div>
+			</div>
+		</section>
+
+		<script type="text/javascript">
+			var entrou1 = 0;
+
+			function animaEscudo1(){
+				if (entrou1 == 0){
+
+					$("#escudo1").removeClass("invisivel");
+					$("#escudo1").addClass("vemEsquerda");
+					$(".fundo").addClass("giraEsquerda");
+
+					entrou1 = 1;
+				}
+				else {
+					$("#escudo1").removeClass("vemEsquerda");
+				}
+			}
+		</script>
 		<!-- Fim do Conteúdo -->
-		
+
 		<?php include('footer.php'); ?>
 
 	</div>
